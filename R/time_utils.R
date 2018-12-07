178952ff58f2ec55255a925e599915ae331aa1a7
quincedaddy <quincedaddy@outlook.com>
2018-12-08 00:21:54 +0900

    Added time_utils.R and removed hello.R

---
+++ R/time_utils.R
@@ -0,0 +1,13 @@
+#' @export
+ConvertMinStrToDec <- function(min_str)
+{
+  Convert <- function(item) {
+    min <- as.numeric(item[1])
+    min <- min + as.numeric(item[2]) / 60
+    round(min, 2)
+  }
+
+  ls <- sapply(str_split(min_str, ":"), Convert)
+  return(ls)
+}
+
