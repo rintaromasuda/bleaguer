82c89ddfc8ca629abb47ccf50d61f4818116fb30
quincedaddy <quincedaddy@outlook.com>
2018-12-10 06:23:34 +0900

    Added initial load of teams and created partial team master data

--- R/load_utils.R
+++ R/load_utils.R
@@ -1,6 +1,10 @@
+#' @export
 LoadTeams <- function()
 {
   file <- system.file("extdata", "teams.csv", package = "bleaguer", mustWork = TRUE)
   df <- read.csv(file, encoding = "UTF-8")
   return(df)
 }
+
+teams <- LoadTeams()
+
