12a673b5d8f2c2c3a64761c5dd44360f472bd811
Rintaro Masuda <rintaromasuda@live.com>
2018-12-10 14:39:35 +0900

    Forgot to include load_utils.R in the last change

--- R/load_utils.R
+++ R/load_utils.R
@@ -2,10 +2,9 @@
 LoadTeams <- function()
 {
   file <- system.file("extdata", "teams.csv", package = "bleaguer", mustWork = TRUE)
-  df <- read.csv(file, fileEncoding = "UTF-8", sep = ",")
+  df <- read.csv(file, encoding = "UTF-8", sep = ",")
 
   return(df)
 }
 
 teams <- LoadTeams()
-
