012eaf67e6c9659149c3496af8b08907e3ef2ea2
Rintaro Masuda <rintaromasuda@live.com>
2018-12-11 14:16:37 +0900

    Using readr package to avoid encoding hell in R on Windows

--- R/load_utils.R
+++ R/load_utils.R
@@ -2,7 +2,7 @@
 LoadTeams <- function()
 {
   file <- system.file("extdata", "teams.csv", package = "bleaguer", mustWork = TRUE)
-  df <- read.csv(file, encoding = "UTF-8", sep = ",")
+  df <- read_csv(file, locale = locale(encoding = "UTF-8"))
 
   return(df)
 }
