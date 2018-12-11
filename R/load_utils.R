6166977883539649997b93602dc32c086a3e51eb
quincedaddy <quincedaddy@outlook.com>
2018-12-12 00:19:12 +0900

    Completed teams.csv and added some test cases for that

--- R/load_utils.R
+++ R/load_utils.R
@@ -2,7 +2,7 @@
 LoadTeams <- function()
 {
   file <- system.file("extdata", "teams.csv", package = "bleaguer", mustWork = TRUE)
-  df <- read_csv(file, locale = locale(encoding = "UTF-8"))
+  df <- readr::read_csv(file, locale = readr::locale(encoding = "UTF-8"))
 
   return(df)
 }
--- tests/testthat/test_load_utils.R
+++ tests/testthat/test_load_utils.R
@@ -1,5 +1,22 @@
 context("Testing load_util")
 
 test_that("Test cases for the team master data frame", {
-  expect_equal(length(unique(teams$TeamId)), 18)
+  expect_equal(
+    nrow(subset(teams, Season == "2016-17" & League == "B1")),
+    18)
+  expect_equal(
+    nrow(subset(teams, Season == "2016-17" & League == "B2")),
+    18)
+  expect_equal(
+    nrow(subset(teams, Season == "2017-18" & League == "B1")),
+    18)
+  expect_equal(
+    nrow(subset(teams, Season == "2017-18" & League == "B2")),
+    18)
+  expect_equal(
+    nrow(subset(teams, Season == "2018-19" & League == "B1")),
+    18)
+  expect_equal(
+    nrow(subset(teams, Season == "2018-19" & League == "B2")),
+    18)
 })
