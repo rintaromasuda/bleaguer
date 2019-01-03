ee3f3845a18892eb3b53e070ae3f0a18aa8dcc41
quincedaddy <quincedaddy@outlook.com>
2019-01-03 21:26:43 +0900

    Created events.csv instead of the code making the same data.

--- R/load_utils.R
+++ R/load_utils.R
@@ -16,25 +16,16 @@ LoadTeams <- function() {
 }
 
 LoadEvents <- function() {
-  col_names <- c("EventId", "Leagues", "Category", "ShortName", "FriendlyName")
-  events <- c(
-    c(2,  "B1",    "Regular", "B1RS",   "B1レギュラーシーズン"),
-    c(3,  "B1",    "Post",    "B1CS",   "B1チャンピオンシップ"),
-    c(4,  "B1",    "Post",    "B1ZPF",  "B1残留プレーオフ"),
-    c(5,  "B1,B2", "AllStar", "AS", "オールスターゲーム"),
-    c(7,  "B2",    "Regular", "B2RS",   "B2レギュラーシーズン"),
-    c(8,  "B2",    "Post",    "B2PF",   "B2プレーオフ"),
-    c(9,  "B2",    "Post",    "B2ZPF",  "B2残留プレーオフ"),
-    c(11, "B1,B2", "Post",    "B1B2SW", "B1/B2入れ替え戦"),
-    c(17, "B2,B3", "Post",    "B2B3SW", "B2/B3入れ替え戦"),
-    c(20, "B1,B2", "Pre",     "EC", "アーリーカップ")
-    )
-  m <- matrix(events, nrow = length(events) / length(col_names),
-              ncol = length(col_names),
-              byrow = TRUE)
-  df <- as.data.frame(m, stringsAsFactors = TRUE)
-  colnames(df) <- col_names
-  df$EventId <- as.integer(as.character(df$EventId))
+  file <- system.file("extdata", "events.csv", package = "bleaguer", mustWork = TRUE)
+  df <- readr::read_csv(file,
+                        col_types = list(
+                          readr::col_integer(),
+                          readr::col_factor(),
+                          readr::col_factor(),
+                          readr::col_character()
+                        ),
+                        locale = readr::locale(encoding = "UTF-8"))
+
   return(df)
 }
 
